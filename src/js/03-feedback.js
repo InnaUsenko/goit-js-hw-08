b3edf8d071b65fa531099042b44866e9605e61b0
InnaUsenko <innausenko8@gmail.com>
2023-08-08 10:57:01 +0000

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
+import throttle from 'lodash.throttle';
 
+const myForm = document.querySelector('.feedback-form');
+const STORAGE_KEY = 'feedback-form-state';
+
+const savedFormState = JSON.parse(localStorage.getItem(STORAGE_KEY));
+if (savedFormState) {
+  myForm.email.value = savedFormState.email;
+  myForm.message.value = savedFormState.message;
+}
+
+const throttleEvent = throttle(formState => {
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formState));
+  console.log(formState);
+}, 500);
+
+myForm.addEventListener('input', event => {
+  const {
+    elements: { email, message },
+  } = event.currentTarget;
+
+  const formState = { email: email.value, message: message.value };
+  throttleEvent(formState);
+});
+
+myForm.addEventListener('submit', event => {
+  event.preventDefault();
+  const {
+    elements: { email, message },
+  } = event.currentTarget;
+
+  const formState = { email: email.value, message: message.value };
+  console.log(formState);
+  localStorage.removeItem(STORAGE_KEY);
+  event.currentTarget.reset();
+});
