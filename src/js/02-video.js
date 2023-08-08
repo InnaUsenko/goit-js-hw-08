f698de1a3ad2a9a2d602966f455327b16b050738
InnaUsenko <innausenko8@gmail.com>
2023-08-08 09:02:41 +0000

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
 
+const player = new Player('vimeo-player');
+const saveCurrentTime = throttle(() => {
+  player.getCurrentTime().then(currentTime => {
+    localStorage.setItem('videoplayer-current-time', currentTime);
+  });
+}, 1000);
+
+player.on('timeupdate', saveCurrentTime);
+
+let currentTime = localStorage.getItem('videoplayer-current-time');
+if (currentTime !== null) {
+  player.setCurrentTime(parseFloat(currentTime));
+}
